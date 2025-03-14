fcc09427335dce7c6af98e673dd82edaa0013f7d
Henry Monteith <hmonteith8365@gmail.com>
2025-03-13 21:03:14 -0400

    Delete app/test directory

--- app/test/annotation.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable
-
-def decorator(**kwargs):
-    def wrapper(func: Callable):
-        def inner():
-            return func(**kwargs)
-        return inner
-    return wrapper
